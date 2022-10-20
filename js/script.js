3bbfa1e20d2d9127573d1bfb1379a78981dbc665
DenisConsta <denisconstantin338@gmail.com>
2022-10-20 15:50:42 +0200

    [add] difficulty selection

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 const grid = document.querySelector('.grid');
-const limit = 100;
-const dim = Math.sqrt(limit);
-const printConsole = (mex) => console.log(mex);
 const btn = document.querySelector('.btn');
+const dimRow = (n) => Math.sqrt(n);
+let isGenerate = false, limit;
 
+//? listener button GENERA
 btn.addEventListener('click', function(){
-  grid.classList.remove('d-none');
+  if(!isGenerate){
+    grid.classList.remove('d-none');
+    isGenerate = true;
+  }else{
+    grid.replaceChildren();
+  }
   initGrid();
 });
 
+// ? Inizializza la griglia
 function initGrid(){
+  limit = parseInt(document.querySelector('select').value);
   for(let i=0; i<limit; i++){
     createCell(i);
   }
 }
 
+// ? Crea le celle nella griglia 
 function createCell(index){
   const cell = document.createElement('div');
-  cell.style.width = `calc(${100}% / ${dim})`;
+  cell.style.width = `calc(${100}% / ${dimRow(limit)})`;
   cell.classList.add('cell');
   cell.innerHTML = index + 1;
-  cell.id = index + 1;
+  cell.idCell = index + 1;
   grid.appendChild(cell);
 
   cell.addEventListener('click', function(){
-    printConsole(this.id);
+    console.log(this.idCell);
     this.classList.add('clicked');
   });
 }
